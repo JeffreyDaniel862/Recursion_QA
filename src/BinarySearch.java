68fd53bb67b0c0d914726c63fad1787fd9a4c04d
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-11-14 11:41:30 +0530

    Binary search solution.

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,21 @@
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,4,5,6,7,8,9,12,14,20};
+        int target = 7;
+        System.out.println(bS(arr, target, 0, arr.length));
+    }
+    static int bS (int[] arr, int target, int start, int end) {
+
+        if (end < start) return -1;
+
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (arr[mid] < target) {
+            return bS(arr, target, mid + 1, end);
+        }
+        return bS(arr, target, start, mid - 1);
+    }
 }
