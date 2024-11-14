b4343830f41bcd78be313817c57b633e429ab47d
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-11-14 19:40:20 +0530

    More methods.

--- src/Array/LinearSearch.java
+++ src/Array/LinearSearch.java
@@ -5,12 +5,15 @@ public class LinearSearch {
         int[] arr = new int[]{1,34,56,76,32,2,4,6,8,90};
         int target = 76;
         System.out.println(findIndex(arr, target, 0));
-
+        System.out.println(isPresent(arr, target, 0));
     }
     private static int findIndex(int[] arr, int target, int index) {
         if (index == arr.length) return -1;
         if (arr[index] == target) return index;
         return findIndex(arr, target, index + 1);
     }
-
+    private static boolean isPresent (int[] arr, int target, int index) {
+        if (index == arr.length) return false;
+        return arr[index] == target || isPresent(arr, target, index + 1);
+    }
 }
