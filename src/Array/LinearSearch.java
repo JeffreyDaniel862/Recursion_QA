f101f266a4b3784a1de63d32af7cafa8d5317fa0
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-11-14 19:39:45 +0530

    Linear search solution using recursion.

---
+++ src/Array/LinearSearch.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,34,56,76,32,2,4,6,8,90};
+        int target = 76;
+        System.out.println(findIndex(arr, target, 0));
+
+    }
+    private static int findIndex(int[] arr, int target, int index) {
+        if (index == arr.length) return -1;
+        if (arr[index] == target) return index;
+        return findIndex(arr, target, index + 1);
+    }
+
+}
