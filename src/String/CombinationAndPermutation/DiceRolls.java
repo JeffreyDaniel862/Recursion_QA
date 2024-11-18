6e2bca72d934d5059612afbbf572f0a86f8c2f6f
JeffreyDaniel862 <jeffreydvictor@gmail.com>
2024-11-18 22:04:49 +0530

    combinations of dice face.

---
+++ src/String/CombinationAndPermutation/DiceRolls.java
@@ -0,0 +1,34 @@
+package String.CombinationAndPermutation;
+
+import java.util.ArrayList;
+
+public class DiceRolls {
+    public static void main(String[] args) {
+        diceCombination("", 4);
+        System.out.println();
+        System.out.println(diceCombinationReturn("", 4));
+    }
+    private static void diceCombination (String processed, int target) {
+        if (target == 0) {
+            System.out.print(processed + ", ");
+            return;
+        }
+
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            diceCombination(processed + i, target - i);
+        }
+    }
+
+    private static ArrayList<String> diceCombinationReturn (String processed, int target) {
+        if (target == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            result.addAll(diceCombinationReturn(processed + i, target - i));
+        }
+        return result;
+    }
+}
